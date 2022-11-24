f5207602a1eb82d2420f1855a96a769db7a85062
rodrigo <rodrigoperez0109@gmail.com>
2022-11-24 10:01:40 -0300

    stamm

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Home() {
+  return (
+    <div className="w-full h-full bg-red-400 grid grid-cols-3 grid"
+    
+    >
+      <div className=''>
+        1
+      </div>
+      <div className=''>
+        1
+      </div>
+      <div className=''>
+        1
+      </div>
+    </div>
+  )
+}
